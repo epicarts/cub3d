9007efc51a258ed5a2554518a2e34316df5f2cdb
YoungHo Choi <0505zxc@gmail.com>
2021-04-14 05:49:38 +0900

    .cub 확장자가 맞는지 확인하는 기능 추가

--- config/arg_parser.c
+++ config/arg_parser.c
@@ -6,11 +6,33 @@
 /*   By: ychoi <ychoi@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 01:52:27 by ychoi             #+#    #+#             */
-/*   Updated: 2021/04/13 03:00:19 by ychoi            ###   ########.fr       */
+/*   Updated: 2021/04/14 05:41:03 by ychoi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+int	check_cub(char *str)
+{
+	char	**list;
+	int		i;
+	char	*tmp;
+
+	list = ft_split(str, '.');
+	i = 0;
+	while (list[i] != NULL)
+	{
+		tmp = list[i];
+		i++;
+	}
+	if (ft_strcmp(tmp, "cub") || i < 2)
+	{
+		free_2d_malloc(list);
+		return (0);
+	}
+	free_2d_malloc(list);
+	return (1);
+}
 
 void	arg_parse(t_info *info, int argc, char *argv[])
 {
@@ -22,5 +44,7 @@ void	arg_parse(t_info *info, int argc, char *argv[])
 		info->screenshot_flag = 1;
 	if (argc == 1)
 		exit_all(info, "인자가 아무것도 없음. 맵을 넣어주세요.", ERROR);
+	if (!check_cub(argv[1]))
+		exit_all(info, "올바른 맵 파일이 아님.", ERROR);
 	info->map_path = argv[1];
 }
